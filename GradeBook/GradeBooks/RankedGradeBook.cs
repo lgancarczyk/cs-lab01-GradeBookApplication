852aa84a48f3c4bb7163d504a5a5888ef43b5663
lgancarczyk <lukgan2000@gmail.com>
2022-03-26 18:47:30 +0100

    Override RankedGradeBook's CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,5 +34,18 @@ namespace GradeBook.GradeBooks
                 return 'D';
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            
+            if (Students.Count<5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
     }
 }
