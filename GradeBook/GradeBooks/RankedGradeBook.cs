d8d84b829ae4e69f0e6d9091dc281b825072fcf8
lgancarczyk <lukgan2000@gmail.com>
2022-03-26 18:12:54 +0100

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,47 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            //return base.GetLetterGrade(averageGrade);
+            if (Students.Count <5)
+            {
+                throw new InvalidOperationException("Can`t be less than 5 students!");
+            }
+
+            int twentyPercent = Students.Count/5;
+            int betterStudents = 0;
+            foreach (var student in Students)
+            {
+                if (averageGrade<= student.AverageGrade)
+                {
+                    betterStudents++;
+                }
+            }
+            if (betterStudents<=twentyPercent)
+            {
+                return 'A';
+            }
+            else if (betterStudents >twentyPercent && betterStudents<= twentyPercent*2)
+            {
+                return 'B';
+            }
+            else if (betterStudents > twentyPercent * 2 && betterStudents <= twentyPercent * 3)
+            {
+                return 'C';
+            }
+            else if (betterStudents > twentyPercent * 3 && betterStudents <= twentyPercent * 4)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+
+
+        }
     }
 }
